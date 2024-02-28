99c6273f38f6079f4bd6a8b6e413a2095440e2b6
Preethigowdaa <159407673+Preethigowdaa@users.noreply.github.com>
2024-02-28 12:20:40 +0530

    Update .hello.java

--- .hello.java
+++ .hello.java
@@ -4,7 +4,8 @@ public class hello
 	public static void main (String args []
 			{
 				System.out.println("Hello world");
-				System.out.println("Hello Java);
+				System.out.println("Hello world);
+		
 			}
 }
 
